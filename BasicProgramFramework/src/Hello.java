33451fbd47bb45f507baeace8d29416a0ae0a221
lenovo <lenovo@192.168.2.2>
2018-06-10 13:40:55 +0530

    added sys

--- BasicProgramFramework/src/Hello.java
+++ BasicProgramFramework/src/Hello.java
@@ -1,8 +1,10 @@
 
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("hello");
+		
 
 	}
 
